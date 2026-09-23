f0cc8297d994eebeef511a266d69c745aac074a1
mohanakrishnan-v <v.mohanakrishnan.07@gmail.com>
2026-09-23 11:59:21 +0530

    Create 1.1.py

---
+++ 1.1.py
@@ -0,0 +1,20 @@
+Week : 1            23.9.26
+1.1 Write a pyhon program for addtion of 2 numbers:
+AIM:
+To add 2 numbers in python
+
+Algorithm:
+step 1: Start
+step 2: Get input from User
+step 3: Add two numbers
+step 4: Display result
+step 5: stop 
+
+Program:
+N1=int(input("Enter First Number:"))
+N2=int(input("Enter Second Number:"))
+sum = N1+N2
+print("Addition of N1 & N2",sum)
+
+Result:
+Thus the program has been executed successfully
